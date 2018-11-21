82bd94d0afd7c876eb635f27d20457a926e1f765
MarinaKuvshinova <pn-05@ukr.net>
2018-11-21 22:46:23 +0200

    update

--- menu.php
+++ menu.php
@@ -2,9 +2,15 @@
     <div class="container-fluid container">
         <div class="collapse navbar-collapse" id="navbar-main">
             <ul class="nav navbar-nav">
-                <li <?=($_GET['page']==1) ? "class='active'":""?>><a href="index.php?page=1">Home</a></li>
-                <li <?=($_GET['page']==2) ? "class='active'":""?>><a href="index.php?page=2">Add</a></li>
-                <li <?=($_GET['page']==3) ? "class='active'":""?>><a href="index.php?page=3">Show</a></li>
+                <?php if(!isset($_GET['page'])):?>
+                    <li class='active'><a href="index.php?page=1">Home</a></li>
+                    <li <?=($_GET['page']==2) ? "class='active'":""?>><a href="index.php?page=2">Add</a></li>
+                    <li <?=($_GET['page']==3) ? "class='active'":""?>><a href="index.php?page=3">Show</a></li>
+                <?php else:?>
+                    <li <?=($_GET['page']==1) ? "class='active'":""?>><a href="index.php?page=1">Home</a></li>
+                    <li <?=($_GET['page']==2) ? "class='active'":""?>><a href="index.php?page=2">Add</a></li>
+                    <li <?=($_GET['page']==3) ? "class='active'":""?>><a href="index.php?page=3">Show</a></li>
+                <?php endif;?>
             </ul>
         </div>
     </div>
